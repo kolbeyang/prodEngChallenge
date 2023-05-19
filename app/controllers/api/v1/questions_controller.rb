981f19d30ba40f8d47cb107744c444b3267d754b
kolbeyang <kky2806@rit.edu>
2023-05-19 09:48:06 -0700

    ask method to communicate with OpenAI API
    Testing to make sure token.env not git ignored

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,6 +1,23 @@
+require 'dotenv'
+Dotenv.load('token.env')
+load_dotenv('.env')
+
 module Api
     module V1
+
+        MAX_TOKENS = 150
+        TEMPERATURE = 0.0
+        MODEL = "text-davinci-003"
+        token  = ENV['TOKEN']
+        client = OpenAI::Client.new(access_token: token)
+
         class QuestionsController < ApplicationController
+
+            def ask(questionContent)
+                prompt = questionContent
+                client.completions(model: MODEL, prompt: prompt, max_tokens: MAX_TOKENS, temperature: TEMPERATURE)
+            end
+
             def index
                 questionContent = params[:question]
                 questions = Question.all
