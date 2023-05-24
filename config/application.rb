7202219b43b5c03f61e0ff2e8a1046ea24c0fcce
kolbeyang <kky2806@rit.edu>
2023-05-24 13:27:49 -0700

    App update to change to rails 6

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
 require_relative "boot"
 
 require "rails/all"
-# require 'dotenv-rails'
-require "ruby/openai"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
